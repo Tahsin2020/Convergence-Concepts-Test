afd49403e8860f91e31430dcc1abc4ce72d78172
Tahsin Hasan <tahsinhasan2@gmail.com>
2023-08-15 12:29:35 -0700

    Initial Scrolling React App for FE task

---
+++ front-end-task/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        {" "}
+        Your text goes here. Also, you can place other elements within this
+        parent div.{" "}
+      </p>
+    </div>
+  );
+}
+
+export default App;
